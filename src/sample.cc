4a59e1da6a0756b1081b5f099e277b73c91222ac
Shigeki Akiyama <shigek.akiyama@gmail.com>
2016-11-07 01:05:43 +0900

    Updated the sample program.

--- src/sample.cc
+++ src/sample.cc
@@ -1,35 +1,37 @@
 #include <cstdio>
 
-__attribute__((noinline))
-double calc(int count)
-{
-    double sum0 = 0.0;
-    double sum1 = 0.0;
-    double sum2 = 0.0;
-    double sum3 = 0.0;
-    double sum4 = 0.0;
-    double sum5 = 0.0;
-    double sum6 = 0.0;
-    double sum7 = 0.0;
-    for (int i = 0; i < count; i+=8) {
-        sum0 += 0.1;
-        sum1 += 0.1;
-        sum2 += 0.1;
-        sum3 += 0.1;
-        sum4 += 0.1;
-        sum5 += 0.1;
-        sum6 += 0.1;
-        sum7 += 0.1;
-    }
+struct A {
+    __attribute__((noinline))
+    static double calc(int count)
+    {
+        double sum0 = 0.0;
+        double sum1 = 0.0;
+        double sum2 = 0.0;
+        double sum3 = 0.0;
+        double sum4 = 0.0;
+        double sum5 = 0.0;
+        double sum6 = 0.0;
+        double sum7 = 0.0;
+        for (int i = 0; i < count; i+=8) {
+            sum0 += 0.1;
+            sum1 += 0.1;
+            sum2 += 0.1;
+            sum3 += 0.1;
+            sum4 += 0.1;
+            sum5 += 0.1;
+            sum6 += 0.1;
+            sum7 += 0.1;
+        }
 
-    return sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7;
-}
+        return sum0 + sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7;
+    }
+};
 
 int main(int argc, char ** argv)
 {
     double r = 0.0;
     for (int i = 0; i < 5; i++)
-        r = calc(1000 * 1000);
+        r = A::calc(1000 * 1000);
 
     std::printf("result = %f\n", r);
 
